81c97eef8eaf2b6a7921e615dc87481572cd4b58
Gabriel Porto <70228491+p0rto@users.noreply.github.com>
2025-10-21 01:41:34 +0000

    feat: add resp parser and echo command

--- app/main.js
+++ app/main.js
@@ -6,8 +6,30 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
-    connection.write(`+PONG\r\n`);
+    const parsedArgs = parseResp(data.toString());
+    for (let i = 0; i < parsedArgs.length; i++) {
+      if (parsedArgs[i].toLowerCase() === "ping") {
+        connection.write("+PONG\r\n");
+      } else if (parsedArgs[i].toLowerCase() === "echo") {
+        const valueToEcho = parsedArgs[i + 1];
+        connection.write(`$${valueToEcho.length}\r\n${valueToEcho}\r\n`);
+      }
+    }
   });
 });
+
+function parseResp(data) {
+  const lines = data.split("\r\n").filter(Boolean);
+  let i = 1;
+  const values = [];
+
+  while (i < lines.length) {
+    if (!lines[i].startsWith("$")) {
+      values.push(lines[i]);
+    }
+    i++;
+  }
+  return values;
+}
 
 server.listen(6379, "127.0.0.1");
