557eace5f7fb3f6dd2a5763dcc55eec546b9cb13
Gabriel Porto <70228491+p0rto@users.noreply.github.com>
2025-10-21 23:13:10 +0000

    feat: implement set and get

--- app/main.js
+++ app/main.js
@@ -1,22 +1,63 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const map = new Map();
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     const parsedArgs = parseResp(data.toString());
-    for (let i = 0; i < parsedArgs.length; i++) {
-      if (parsedArgs[i].toLowerCase() === "ping") {
-        connection.write("+PONG\r\n");
-      } else if (parsedArgs[i].toLowerCase() === "echo") {
-        const valueToEcho = parsedArgs[i + 1];
-        connection.write(`$${valueToEcho.length}\r\n${valueToEcho}\r\n`);
-      }
+    console.log(parsedArgs);
+    let output;
+    switch (parsedArgs[0].toLowerCase()) {
+      case "ping":
+        output = handlePing();
+        break;
+      case "echo":
+        output = handleEcho(parsedArgs);
+        break;
+      case "set":
+        output = handleSet(parsedArgs);
+        console.log("output", output);
+        break;
+      case "get":
+        output = handleGet(parsedArgs);
+        break;
+      default:
+        return null;
     }
+
+    connection.write(output);
   });
 });
+
+function handlePing() {
+  return "+PONG\r\n";
+}
+
+function handleEcho(data) {
+  if (data.length < 2) {
+    throw new Error("Missing args");
+  }
+  return `$${data[1].length}\r\n${data[1]}\r\n`;
+}
+
+function handleSet(data) {
+  if (data.length < 3) {
+    throw new Error("Missing args");
+  }
+
+  map.set(data[1], data[2]);
+  return "+OK\r\n";
+}
+
+function handleGet(data) {
+  if (data.length < 2) {
+    throw new Error("Missing args");
+  }
+  const val = map.get(data[1]);
+
+  return val ? `$${val.length}\r\n${val}\r\n` : "$-1\r\n";
+}
 
 function parseResp(data) {
   const lines = data.split("\r\n").filter(Boolean);
